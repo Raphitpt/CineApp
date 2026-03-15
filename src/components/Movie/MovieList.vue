c9bb00a96917bd338c23af099030474dcbbf725d
Raphaël Tiphonet <tiphonetraphi@gmail.com>
2026-03-15 10:22:08 +0100

    fix: convert composition api to option api

--- src/components/Movie/MovieList.vue
+++ src/components/Movie/MovieList.vue
@@ -1,22 +1,34 @@
-<script setup lang="ts">
-import { ref, computed, onMounted, watch } from "vue";
+<script lang="ts">
+import { defineComponent } from "vue";
 import { useMoviesStore } from "@/stores/movieStore";
+import { mapState } from "pinia";
 import MovieFilter from "./MovieFilter.vue";
 import Movie from "./Movie.vue";
 
-const store = useMoviesStore();
-onMounted(() => store.fetchMovies());
-
-const renderedMovies = ref(store.movies);
-
-watch(
-  () => store.movies,
-  (movies) => {
-    renderedMovies.value = [...movies];
+export default defineComponent({
+  components: { MovieFilter, Movie },
+  data() {
+    return {
+      renderedMovies: [] as ReturnType<typeof useMoviesStore>["movies"],
+    };
   },
-);
-
-const moviesLength = computed(() => renderedMovies.value.length);
+  computed: {
+    ...mapState(useMoviesStore, ["movies", "loading", "error"]),
+    moviesLength() {
+      return this.renderedMovies.length;
+    },
+  },
+  watch: {
+    movies(newMovies) {
+      this.renderedMovies = [...newMovies];
+    },
+  },
+  mounted() {
+    const store = useMoviesStore();
+    store.fetchMovies();
+    this.renderedMovies = [...store.movies];
+  },
+});
 </script>
 
 <template>
@@ -26,11 +38,11 @@ const moviesLength = computed(() => renderedMovies.value.length);
       <p class="text-slate-500 mt-2">{{ moviesLength }} films disponibles</p>
     </header>
 
-    <MovieFilter :movies="store.movies" @update-movies="renderedMovies = $event" />
+    <MovieFilter :movies="movies" @update-movies="renderedMovies = $event" />
 
-    <div v-if="store.loading" class="text-center text-slate-400 mt-20">Chargement...</div>
+    <div v-if="loading" class="text-center text-slate-400 mt-20">Chargement...</div>
 
-    <p v-else-if="store.error" class="text-center text-red-500 mt-20">Erreur : {{ store.error }}</p>
+    <p v-else-if="error" class="text-center text-red-500 mt-20">Erreur : {{ error }}</p>
 
     <div
       v-else-if="renderedMovies.length > 0"
